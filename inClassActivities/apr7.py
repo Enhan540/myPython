3b1f196b271f27044eb05bfecaed4cb63c921123
Enhan540 <147754751+Enhan540@users.noreply.github.com>
2026-04-07 14:36:06 -0700

    Update apr7.py

--- inClassActivities/apr7.py
+++ inClassActivities/apr7.py
@@ -1,1 +1,23 @@
+# Student and Roster classes
+class Student:
+  def __init__(self, first, last, gpa):
+    self.first = first
+    self.last = last
+    self.gpa = gpa
+  def get_last(self):
+    return self.last
+  def get_gpa(self):
+    return self.gpa
 
+class Roster:
+  def __init__(self):
+    self.roster = []
+  def add_student(self, student):
+    self.roster.append(student)
+  def course_size(self):
+    return len(self.roster)
+  def get_deans_list(self):
+    # return last name of all students on dean's list
+    for student in self.roster:
+      if (student.get_gpa >= 3.5):
+        return
